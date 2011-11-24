82ac00a517b4456f3151fc9ff3496234de1c6eac
Michael Homer <mwh@mwh.geek.nz>
2011-11-24 21:43:16 +1300

    Fix second re error

--- retail.c
+++ retail.c
@@ -29,7 +29,7 @@ int tail_regex_unseekable(FILE *fp, char *pattern) {
         int len = regerror(rcv, &re, NULL, 0);
         char *estr = malloc(len);
         regerror(rcv, &re, estr, len);
-        fprintf(stderr, "Error compiling regex: %s\n");
+        fprintf(stderr, "Error compiling regex: %s\n", estr);
         exit(1);
     }
     int lines_size = 10;
